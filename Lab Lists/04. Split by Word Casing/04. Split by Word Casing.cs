74e4f9029d05cc69a25c7009cc3d215006ffa2ef
Warglaive <warglaivegm@gmail.com>
2017-03-25 18:18:33 +0200

    PCListExcercicesUpdate

---
+++ Lab Lists/04. Split by Word Casing/04. Split by Word Casing.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Split_by_Word_Casing
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var text = Console.ReadLine()
+                .Split(new char[] { ' ', ',', ';', ':', '.', '!', '(', ')', '"', '\'', '\\', '/', '[', ']' }
+                , StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            var lowerCase = new List<string>();
+            var mixedCase = new List<string>();
+            var upperCase = new List<string>();
+            bool isUpper = true;
+            bool isLower = true;
+            foreach (var word in text)
+            {
+                isUpper = true;
+                isLower = true;
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (char.IsUpper(word[i]))
+                    {
+                        isLower = false;
+                    }
+                    else if (char.IsLower(word[i]))
+                    {
+                        isUpper = false;
+                    }
+                    else
+                    {
+                        isLower = false;
+                        isUpper = false;
+                    }
+                }
+                if (isUpper)
+                {
+                    upperCase.Add(word);
+                }
+                else if (isLower)
+                {
+                    lowerCase.Add(word);
+                }
+                else
+                {
+                    mixedCase.Add(word);
+                }
+            }
+            Console.WriteLine("Lower-case: {0}", string.Join(", ", lowerCase));
+            Console.WriteLine("Mixed-case: {0}", string.Join(", ", mixedCase));
+            Console.WriteLine("Upper-case: {0}", string.Join(", ", upperCase));
+        }
+    }
+}
---
+++ Lab Lists/07. Count Numbers/07. Count Numbers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Count_Numbers
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            bool didSwap = false;
+            var counts = new int[numbers.Max() + 1];
+            //sort numbers
+            do
+            {
+                for (int i = 0; i < numbers.Count - 1; i++) 
+                {
+                    didSwap = false;
+                    if (numbers[i] > numbers[i + 1]) 
+                    {
+                        var temp = numbers[i + 1];
+                        numbers[i + 1] = numbers[i];
+                        numbers[i] = temp;
+                        didSwap = true;
+                    }
+                }
+            } while (didSwap);
+            //Count numbers
+            foreach (var num in numbers)
+            {
+                counts[num]++;
+            }
+            for (int i = 0; i < counts.Length; i++)
+            {
+                if (counts[i] > 0 )
+                {
+                    Console.WriteLine($"{i} -> {counts[i]}");
+                }
+            }
+        }
+    }
+}
